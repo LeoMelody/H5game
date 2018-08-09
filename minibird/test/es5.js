62c98bd56c63be40429caae69a3d5b36b2a25079
leoMelody <760282292@qq.com>
2018-08-09 15:53:23 +0800

    面向对象编程

--- minibird/test/es5.js
+++ minibird/test/es5.js
@@ -64,7 +64,7 @@
 
   // 组合继承，JS完美模拟继承一代目 经过研究学习，造出大象类🐘，并改造了Animal类 -> GreetAnimal
   const GreetAnimal = function(name, age) {
-    console.log('我被调用了')
+    // console.log('我被调用了')
     // 基础参数和初始化数据放在构造函数中
     this.name = name
     this.age = age
@@ -77,7 +77,7 @@
   }
 
   GreetAnimal.prototype.eat = function() {
-    // this.initFood.pop()
+    this.initFood.pop()
   }
 
   const Elephant = function() {
@@ -139,4 +139,68 @@
   console.log(sonObj2)
   // 但是，原型模式我认为不能被完全成为是继承。因为继承是面向类的，即使是伪造的类也要有个类的样子
   // 原型模式只是实现了父实例的copy操作和原型链的延伸操作，可是sonObj没有构造函数了（也就是没有一个抽象化的初始化对象的方法了）
+  // 如果仅仅是为了copy对象，不需要构造函数的情况下，Object.create() 看起来是那么的美好
+
+  // 寄生式继承 高程中的寄生继承例子不太明显，感觉和原型式继承没什么差别？
+  // 在我实践之前，我一度认为寄生构造函数更像是继承
+  // 这里我以寄生构造函数 实现 Ant类 
+  function Ant() {
+    let greetAnimal = new GreetAnimal(...arguments) // 将父类实例寄生在子类构造函数中
+    greetAnimal.legs = 8
+    return greetAnimal
+  }
+
+  let ant1 = new Ant('小蚂蚁', 9)
+  ant1.eat()
+  console.log(ant1)
+  console.log(new Ant('my2', 18))
+  // 但是，这其实已经不能算是继承了。这里我发现 Ant的实例 ant1 和 GreetAnimal的实例greetAnimal居然是同一级（或者说就是一个对象）
+  // 这其实是对GreetAnimal的扩展
+
+  // 那好，既然这样，我再换种方式：
+  function createAnt() {
+    let ant = new GreetAnimal(...arguments)
+    function F() {}
+    F.prototype = ant
+    return new F()
+  }
+
+  let antson = createAnt('子类蚂蚁', 18)
+  console.log(antson)
+  // 这样也变相的实现了继承。antson的原型对象为 GreetAnimal的实例
+  // 问题： 如果基础属性在antson上而不是其原型对象上 而且antson有一个真正的属于自己的构造函数就完美了
+
+
+  // 寄生组合继承
+  // 根据上面的这个需求，其实哪个F如果可以指定构造函数就很接近完美了，继续改造：
+  // 这次以鱼Fish为例
+  const Fish = function() {
+    GreetAnimal.call(this, ...arguments) // 构造函数完成属性copy
+    this.legs = 0
+  }
+  /**
+   *这个方法是实现寄生组合继承的核心
+   * @param {*} subType 子类构造函数
+   * @param {*} superType 超类构造函数
+   */
+  function inheritProto(subType, superType) {
+    // 伪造一个tmp的构造函数 目的只是利用其prototype属性来缓存父类prototype
+    function F() {}
+    // 缓存prototype。这一步真的是画龙点睛之笔。通过这个操作，F成为了和superType同一级（具有相同的原型对象）
+    // 且非常干净的存在（没有多余的属性，因为它最开始就足够干净）
+    F.prototype = superType.prototype
+    // 此时，通过new操作符创造出的F实例，数据属性干净，方法由于是原型对象的属性，所以又可以共享，简直棒
+    // 这个对象满足了在JS中作为一个原型对象（该有的有，该没有的没有）所需要的特质
+    let prototype = new F()
+    // 将这个对象变为子类的原型对象
+    subType.prototype = prototype
+    prototype.constructor = subType
+  }
+
+  inheritProto(Fish, GreetAnimal)
+
+  let fish = new Fish('xy', 10) // JS中完美的继承方案
+  console.log(fish) // 完美的鱼类
+
+  // 其实不完美，完美的话它就是人了。这里还是使用了较为
 })()
